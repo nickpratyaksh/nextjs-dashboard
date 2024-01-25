8f5b9f247e6657919c1acb364afaf1af3c64b26d
nickpratyaksh <nickpratyaksh@gmail.com>
2024-01-26 00:37:27 +0530

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/dashboard-app/creating-layouts-and-pages" className="text-blue-500">
+            <a href="https://nextjs.org/learn/dashboard-app/partial-prerendering" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
